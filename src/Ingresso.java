760f42175cbbd7c991e17986ae43c64eb73d5822
Breno Luna <breno.luna_@hotmail.com>
2019-04-09 16:15:54 -0300

    primeira interacao cliente-servidor

--- src/Ingresso.java
+++ src/Ingresso.java
@@ -1,9 +1,32 @@
 
 public class Ingresso {
+	private static int id;
 	
 	private int codigo;
 	private Sessao sessao;
 	private int numCadeira;
 	
+	public Ingresso(Sessao sessao, int numCadeira) {
+		this.codigo = id++;
+		this.sessao = sessao;
+		this.numCadeira = numCadeira;
+		
+	}
+
+	public Sessao getSessao() {
+		return sessao;
+	}
+
+	public void setSessao(Sessao sessao) {
+		this.sessao = sessao;
+	}
+	
+	public int getNumCadeira() {
+		return this.numCadeira;
+	}
+	
+	public void setNumCadeira(int numCadeira) {
+		this.numCadeira = numCadeira;
+	}
 
 }
--- src/Sessao.java
+++ src/Sessao.java
@@ -1,7 +1,73 @@
+import java.util.ArrayList;
 
 public class Sessao {
 	private int hora;
 	private int quantidadeCadeiras;
 	private Cadeira[] cadeiras;
+	
+	
+	
+	public Sessao(int hora, int quantidadeCadeiras) {
+		this.hora = hora;
+		this.quantidadeCadeiras = quantidadeCadeiras;
+		cadeiras = cadeiraGenerator(quantidadeCadeiras);
+	}
+	
+	
+	
+	
+	public int getHora() {
+		return hora;
+	}
+
+
+
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+
+
+
+	public int getQuantidadeCadeiras() {
+		return quantidadeCadeiras;
+	}
+
+
+
+	public void setQuantidadeCadeiras(int quantidadeCadeiras) {
+		this.quantidadeCadeiras = quantidadeCadeiras;
+	}
+
+
+
+	public Cadeira[] getCadeiras() {
+		return cadeiras;
+	}
+
+
+
+	public void setCadeiras(Cadeira[] cadeiras) {
+		this.cadeiras = cadeiras;
+	}
+
+
+
+	public Cadeira[] cadeiraGenerator(int quantidade) {
+		Cadeira cadeiras[] = new Cadeira[quantidade];
+		for(int i = 0; i < quantidade; i++) {
+			cadeiras[i].setDesocupada(true);
+		}
+		return cadeiras;
+	}
+	
+	public ArrayList<Integer> cadeirasVagas(){
+		ArrayList<Integer> cadeirasVagas = new ArrayList<Integer>();
+		for(int i = 0; i < this.quantidadeCadeiras; i++) {
+			if(this.cadeiras[i].isDesocupada()) {
+				cadeirasVagas.add(i);
+			}
+		}
+		return cadeirasVagas;
+	}
 
 }
